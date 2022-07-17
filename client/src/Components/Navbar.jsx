65993ea062e49bcb9bf41f00dfa690785ec296b2
PrafulDeore <91863686+PrafulDeore@users.noreply.github.com>
2022-07-17 16:58:49 +0530

    a

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -11,7 +11,8 @@ export default function Navbar() {
     <div className='nav' style={{}}>
 
       <div style={{ display: "flex" }}>
-        <img className='logo' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSN8cxwk65-TwH9z6VLuEdT1cx9jJEUl8xZEFiUAaIuhIU5DtKMM_UMiFa51CB_aKE74b8&usqp=CAU" alt="" />
+        <img className='logo' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSN8cxwk65-TwH9z6VLuEdT1cx9jJEUl8xZEFiUAaIuhIU5DtKMM_UMiFa51CB_aKE74b8&usqp=CAU" alt=""
+         />
         <h1 className="masai_name">MASAI PIZZA</h1>
       </div>
 
